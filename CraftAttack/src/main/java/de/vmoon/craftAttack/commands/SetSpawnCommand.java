2e26cfdfe633a969e9a86bdcc7b98163e49ea0cb
VoidableMoon884 <trash_02@nogacity.de>
2025-08-26 14:22:06 +0200

    Version 1.9.2 JSON verifyer New setspawn logic

--- CraftAttack/src/main/java/de/vmoon/craftAttack/commands/SetSpawnCommand.java
+++ CraftAttack/src/main/java/de/vmoon/craftAttack/commands/SetSpawnCommand.java
@@ -1,45 +1,92 @@
 package de.vmoon.craftAttack.commands;
 
+import de.vmoon.craftAttack.CraftAttack;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.Location;
 
 public class SetSpawnCommand implements CommandExecutor {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // Berechtigungsprüfung
-        if (!sender.hasPermission("ca.admin.spawn")) {
-            sender.sendMessage(ChatColor.RED + "Du hast keine Berechtigung, diesen Befehl zu verwenden.");
-            return true;
-        }
+        // 1) Nur Spieler mit Permission
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "Diesen Befehl kannst du nur als Spieler ausführen.");
+            sender.sendMessage(ChatColor.RED + "Nur ein Spieler kann das ausführen.");
             return true;
         }
         Player player = (Player) sender;
-
-        // Bei richtiger Ausführung erwarten wir keine weitere Argumente (weil der Sub-Befehl bereits entfernt wurde)
-        if (args.length != 0) {
-            player.sendMessage(ChatColor.RED + "Usage: /craftattack setspawn (ohne zusätzliche Argumente)");
+        if (!player.hasPermission("ca.admin.spawn")) {
+            player.sendMessage(ChatColor.RED + "Dazu hast du keine Rechte.");
             return true;
         }
 
-        // Erstelle einen Stone Axe als Auswahlwerkzeug
-        ItemStack tool = new ItemStack(Material.STONE_AXE, 1);
-        ItemMeta meta = tool.getItemMeta();
-        meta.setDisplayName(ChatColor.GREEN + "Spawn-Auswahl Werkzeug");
-        tool.setItemMeta(meta);
+        // 2) Wir erwarten genau 4 Argumente: x y z radius
+        if (args.length != 4) {
+            player.sendMessage(ChatColor.RED +
+                    "Usage: /ca setspawn <x> <y> <z> <radius>");
+            return true;
+        }
 
-        // Verteile das Werkzeug dem Spieler und informiere ihn
-        player.getInventory().addItem(tool);
-        player.sendMessage(ChatColor.GREEN + "Du hast das Spawn-Auswahl Werkzeug erhalten.");
-        player.sendMessage(ChatColor.GREEN + "Markiere mit Links- oder Rechtsklick drei Ecken des gewünschten Bereichs. Die Koordinaten der angeklickten Blöcke werden im Chat ausgegeben.");
+        Location loc = player.getLocation();
+        double x, y, z, r;
+        try {
+            x = parseArg(args[0], loc.getX());
+            y = parseArg(args[1], loc.getY());
+            z = parseArg(args[2], loc.getZ());
+            r = Double.parseDouble(args[3]);
+            if (r < 0) throw new NumberFormatException();
+        } catch (NumberFormatException ex) {
+            player.sendMessage(ChatColor.RED +
+                    "Koordinaten müssen Zahl oder '~' sein. Radius muss positive Zahl sein.");
+            return true;
+        }
+
+        // 3) Teleport-Punkt speichern
+        CraftAttack plugin = CraftAttack.getInstance();
+        plugin.getConfig().set("teleport.x", x);
+        plugin.getConfig().set("teleport.y", y);
+        plugin.getConfig().set("teleport.z", z);
+
+        // 4) spawnArea Ecken berechnen und speichern
+        double x1 = x - r, y1 = y - r, z1 = z - r;
+        double x2 = x + r, y2 = y + r, z2 = z + r;
+        plugin.getConfig().set("spawnArea.x1", x1);
+        plugin.getConfig().set("spawnArea.y1", y1);
+        plugin.getConfig().set("spawnArea.z1", z1);
+        plugin.getConfig().set("spawnArea.x2", x2);
+        plugin.getConfig().set("spawnArea.y2", y2);
+        plugin.getConfig().set("spawnArea.z2", z2);
+
+        plugin.saveConfig();
+
+        // 5) Feedback an Spieler
+        player.sendMessage(ChatColor.GREEN + "Teleport-Punkt gesetzt: " +
+                formatVec(x, y, z));
+        player.sendMessage(ChatColor.GREEN + "Spawn-Bereich (Radius " + r + "):");
+        player.sendMessage(ChatColor.GREEN +
+                "Ecke1 " + formatVec(x1, y1, z1) +
+                "  Ecke2 " + formatVec(x2, y2, z2));
 
         return true;
+    }
+
+    /**
+     * Parst eine Koordinate: "~" -> current, sonst Double.parse
+     */
+    private double parseArg(String arg, double current) {
+        if ("~".equals(arg)) {
+            return current;
+        }
+        return Double.parseDouble(arg);
+    }
+
+    private String formatVec(double x, double y, double z) {
+        return "(" +
+                String.format("%.1f", x) + ", " +
+                String.format("%.1f", y) + ", " +
+                String.format("%.1f", z) + ")";
     }
 }
--- CraftAttack/src/main/java/de/vmoon/craftAttack/utils/ConfigManager.java
+++ CraftAttack/src/main/java/de/vmoon/craftAttack/utils/ConfigManager.java
@@ -88,30 +88,6 @@ public class ConfigManager {
         return getConfig().getBoolean("tabtext", true);
     }
 
-    public int getSpawnAreaX1() {
-        return getConfig().getInt("spawnArea.x1", 0);
-    }
-
-    public int getSpawnAreaY1() {
-        return getConfig().getInt("spawnArea.y1", 0);
-    }
-
-    public int getSpawnAreaZ1() {
-        return getConfig().getInt("spawnArea.z1", 0);
-    }
-
-    public int getSpawnAreaX2() {
-        return getConfig().getInt("spawnArea.x2", 0);
-    }
-
-    public int getSpawnAreaY2() {
-        return getConfig().getInt("spawnArea.y2", 0);
-    }
-
-    public int getSpawnAreaZ2() {
-        return getConfig().getInt("spawnArea.z2", 0);
-    }
-
     // Methoden für die Extra-Konfiguration (z. B. playerstatuses.yml)
 
     /**
